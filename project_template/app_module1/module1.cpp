6e318b694bb41aceed2069a6835b29c00ea71c02
Adrian <info@kuhlbrodt.org>
2023-09-26 19:07:25 +0200

    changes

---
+++ project_template/app_module1/module1.cpp
@@ -0,0 +1,13 @@
+#include "module1.h"
+
+namespace template_project
+{
+    namespace app_module1
+    {
+        int RunModule1()
+        {
+            std::cout << "Module 1 is running" << std::endl;
+            return 0;
+        }
+    }
+}
---
+++ project_template/app_module1/module1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+namespace template_project
+{
+    namespace app_module1
+    {
+        int RunModule1();
+    }
+}
